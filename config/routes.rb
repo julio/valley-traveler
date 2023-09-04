fdeafd1e504103c78034efbbbbde1eebb300d491
Julio Santos <julio@morgane.com>
2023-09-04 12:31:13 -0700

    link to posts from index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
