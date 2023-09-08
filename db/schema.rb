04545903fba15e3a747f91ebfe025625f11a4242
Julio Santos <julio@morgane.com>
2023-09-08 10:14:58 -0700

    add published/scheduled status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_174227) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_012807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_174227) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
