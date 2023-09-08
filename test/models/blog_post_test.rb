54cdbb22d9866b83f21c63208c19308cf3df8f55
Julio Santos <julio@morgane.com>
2023-09-08 11:28:09 -0700

    Add tests for BlogPost model

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft if published_at not set" do
+    blog_post = BlogPost.new(title: "title", body: "body")
+    assert blog_post.draft?
+  end
+
+  test "not draft if published_at set" do
+    blog_post = BlogPost.new(title: "title", body: "body", published_at: 3.days.ago)
+    refute blog_post.draft?
+  end
+
+  test "published if published_at set in the past" do
+    blog_post = BlogPost.new(title: "title", body: "body", published_at: 3.days.ago)
+    assert blog_post.published?
+  end
+
+  test "not published if published_at set in the future" do
+    blog_post = BlogPost.new(title: "title", body: "body", published_at: 3.days.from_now)
+    refute blog_post.published?
+  end
+
+  test "scheduled if published_at set in the future" do
+    blog_post = BlogPost.new(title: "title", body: "body", published_at: 3.days.from_now)
+    assert blog_post.scheduled?
+  end
 end
